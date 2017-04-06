d69612f2605ae168c150e2ea7d5c07d1b6a47688
chunankong <chenxu@dhcp-10-105-129-179.wireless.northwestern.private>
2017-04-05 22:58:00 -0500

    fuckmylife

--- ID3.py
+++ ID3.py
@@ -1,7 +1,7 @@
 from node import Node
 import math
 
-usedatt = 0
+usedatt = []
 def ID3(examples, default):
   '''
   Takes in an array of examples, and returns a tree (an instance of Node) 
@@ -11,7 +11,11 @@ def ID3(examples, default):
   '''
   tree = []
   classfc=[]
+  yexamples = []
+  nexamples = []
   A = Node()
+  modeA = Node()
+  tree = Node()
   for each in examples:
     classfc.append(each.get('Class'))
 #if there is no more items in the list
@@ -25,7 +29,7 @@ def ID3(examples, default):
     A.children=None
     return A
 #if we ran out of possible trees
-  elif usedatt == 16:
+  elif len(usedatt) == 16:
     numD=0
     numR=0
     for each in examples:
@@ -36,15 +40,64 @@ def ID3(examples, default):
     if numD>numR:
       A.label='democrat'
       A.children=None
+      modeA = A
       return A
     else:
       A.label='republican'
       A.children=None
+      modeA = A
       return A
 # the general case.
+# the first children is the node of next category with selection 'n'
+# the second children is the node of next category with the selection 'y'
   else:
+    bestL= getBestL(examples,default)
+    usedatt.append(bestL)
+    for each in examples:
+      if each.get(bestL) == 'y':
+        yexamples.append(each)
+      else:
+        nexamples.append(each)
+    ysubtree = ID3(yexamples, modeA.label)
+    nsubtree = ID3(nexamples, modeA.label)
+    tree.label = bestL
+    children[0] = nsubtree
+    children[1] = ysubtree
     return tree
+
+def getBestL(examples, default):
+  '''
+  helper function that returns the best label for the root node
+  '''
+  myatts = examples.keys()
+  myatts.remove('Class')
+  total = len(examples)
+  mystats = []
+## the following gets a list of dictionaries with key=label and value = list of difference
+## in votings number of d-number of r in the order of n, y
+  for each in myatts:
+    eachlist = []
+    eachyr = 0
+    eachyd = 0
+    eachnr = 0
+    eachnd = 0
+    if each not in usedatt:
+      for example in examples:
+      if example.get(each) == 'y' and example.get('Class') == 'democrat':
+        eachyd += 1
+      elif example.get(each) == 'y' and example.get('Class') == 'republican':
+        eachyr += 1
+      elif example.get(each) == 'n' and example.get('Class') == 'democrat':
+        eachnd += 1
+      elif example.get(each) == 'n' and example.get('Class') =='republican':
+        eachnr += 1
+    eachlist.append((eachnd-eachnr)/(eachnd+eachnr))
+    eachlist.append((eachyd-eachyr)/(eachyd+eachyr))
+    mystats.append({each, eachlist})
+##
   
+  
+
 def prune(node, examples):
   '''
   Takes in a trained tree and a validation set of examples.  Prunes nodes in order
@@ -56,6 +109,13 @@ def test(node, examples):
   Takes in a trained tree and a test set of examples.  Returns the accuracy (fraction
   of examples the tree classifies correctly).
   '''
+  accr = 1
+  total = len(examples)
+  for each in examples:
+    myresult = evaluate(node, each)
+    if myresult == each.get('Class'):
+      accr += 1
+  return accr/total 
 
 
 def evaluate(node, example):
