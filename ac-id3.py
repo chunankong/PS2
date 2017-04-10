d4198b8266c3426c7fa59d28fed60c6a5603c693
Anqi Chen <acmeow@users.noreply.github.com>
2017-04-10 18:33:49 -0500

    id3.py finished

--- ac-id3.py
+++ ac-id3.py
@@ -1,6 +1,7 @@
 from node import Node
 import math
-import random
+
+CLASS_ATTR = 'Class'
 
 def ID3(examples, default):
     '''
@@ -9,26 +10,35 @@ def ID3(examples, default):
     pairs, and the target class variable is a special attribute with the name
     "Class". Any missing attributes are denoted with a value of "?"
     '''
+    # return default value if the example set is empty
     if not examples:
         return Node(default)
 
-    target_vals = [example['Class'] for example in examples]
-    if target_vals.count(target_vals[0]) == len(target_vals):
-        print 'enter len check'
-        return Node(target_vals[0])
+    class_vals = [example[CLASS_ATTR] for example in examples]
+    # return the classification if all examples have the same class value
+    if class_vals.count(class_vals[0]) == len(class_vals):
+        return Node(class_vals[0])
     else:
-        attrs = [key for key in examples[0].keys() if key != 'Class']
-        best_attr = choose_attr(examples, 'Class', attrs)
+        attrs = [attr for attr in examples[0].keys() if attr != CLASS_ATTR]
+        # return the most common class value if attribute set is empty
+        if not attrs:
+            return Node(find_majority(examples))
+
+        # create a new node with the best attribute as label
+        best_attr = choose_attr(examples, attrs)
         node = Node(best_attr)
+        node.majority = find_majority(examples)
 
         for val in get_values(examples, best_attr):
+            # filter a subset
             subset = []
             for example in examples:
-                if best_attr in example and example[best_attr] == val:
-                    subset.append(example)
-
-            for entry in subset:
-                del entry[best_attr]
+                if example[best_attr] == val:
+                    new_example = {}
+                    for example_attr, example_val in example.items():
+                        if example_attr != best_attr:
+                            new_example[example_attr] = example_val
+                    subset.append(new_example)
 
             subtree = ID3(subset, default)
             node.children[val] = subtree
@@ -36,17 +46,56 @@ def ID3(examples, default):
         return node
 
 def prune(node, examples):
-  '''
-  Takes in a trained tree and a validation set of examples.  Prunes nodes in order
-  to improve accuracy on the validation data; the precise pruning strategy is up to you.
-  '''
+    '''
+    Takes in a trained tree and a validation set of examples. Prunes nodes in
+    order to improve accuracy on the validation data; the precise pruning
+    strategy is up to you. Reduced Error Pruning used.
+    '''
+    acc_table = {}
+    nodes = [node]
+
+    while nodes:
+        curr_node = nodes.pop()
+        # skip leaf nodes
+        if not curr_node.children:
+            continue
+
+        nodes.extend([child for child in curr_node.children.values()])
+        children_save = curr_node.children
+        label_save = curr_node.label
+
+        # change current node to leaf node and test accuracy
+        curr_node.children = {}
+        curr_node.label = curr_node.majority
+        acc_table[curr_node] = test(node, examples)
+
+        curr_node.children = children_save
+        curr_node.label = label_save
+
+    curr_acc = test(node, examples)
+    best_acc = curr_acc
+    prune_node = None
+    for key, val in acc_table.items():
+        if val > best_acc:
+            best_acc = val
+            prune_node = key
+
+    # prune the node with better accuracy
+    if best_acc != curr_acc:
+        prune_node.label = prune_node.majority
+        prune_node.children = {}
+        prune(node, examples)
 
 def test(node, examples):
-  '''
-  Takes in a trained tree and a test set of examples.  Returns the accuracy (fraction
-  of examples the tree classifies correctly).
-  '''
-
+    '''
+    Takes in a trained tree and a test set of examples.  Returns the accuracy
+    (fraction of examples the tree classifies correctly).
+    '''
+    success = 0.0
+    for example in examples:
+        if evaluate(node, example) == example[CLASS_ATTR]:
+            success += 1.0
+    return success / len(examples)
 
 def evaluate(node, example):
     '''
@@ -56,11 +105,18 @@ def evaluate(node, example):
     if not node.children:
         return node.label
 
+    # handle unknown attribute values
+    if example[node.label] not in node.children.keys():
+        keys = node.children.keys()
+        return evaluate(node.children[keys[0]], example)
+    else:
+        return evaluate(node.children[example[node.label]], example)
+
 ############################## HELPER FUNCTIONS ###############################
 
 def calc_entropy(examples, attr):
     '''
-    Calculates entropy of the given attribute.
+    Return entropy of the given attribute.
     '''
     val_freq = {}
     for example in examples:
@@ -76,47 +132,85 @@ def calc_entropy(examples, attr):
 
     return entropy
 
-def calc_infogain(examples, target_attr, attr):
+def calc_infogain(examples, attr):
     '''
-    Calculates informaiton gain of the given attribute.
+    Return informaiton gain of the given attribute.
     '''
     val_freq = {}
+    missing_attr = 0.0
     for example in examples:
         if example[attr] == '?':
-            example[attr] = random.choice('yn')
-
-        if example[attr] in val_freq:
+            missing_attr += 1.0
+        elif example[attr] in val_freq:
             val_freq[example[attr]] += 1.0
         else:
             val_freq[example[attr]] = 1.0
+
+    # handle missing attributes
+    if missing_attr != 0.0:
+        # find the majority value
+        majority_val = None
+        majority_freq = 0.0
+        for val, freq in val_freq.items():
+            if freq > majority_freq:
+                majority_val = val
+                majority_freq = freq
+
+        # swap missing attributes with the majority value
+        for example in examples:
+            if example[attr] == '?':
+                example[attr] = majority_val
+
+        # update frequency table
+        val_freq[majority_val] += missing_attr
 
     subset_entropy = 0.0
     for val in val_freq.values():
         val_prob = val / sum(val_freq.values())
         subset = [example for example in examples if example[attr] == val]
-        subset_entropy += val_prob * calc_entropy(subset, target_attr)
+        subset_entropy += val_prob * calc_entropy(subset, CLASS_ATTR)
 
+    return calc_entropy(examples, CLASS_ATTR) - subset_entropy
 
-    return calc_entropy(examples, target_attr) - subset_entropy
-
-def choose_attr(examples, target_attr, attrs):
+def choose_attr(examples, attrs):
     '''
-    Choose the best attribute to split on based on information gain.
+    Return the best attribute to split on based on information gain.
     '''
     best_attr = None
     best_infogain = 0.0
-
     for attr in attrs:
-        infogain = calc_infogain(examples, target_attr, attr)
-        if infogain >= best_infogain:
+        infogain = calc_infogain(examples, attr)
+        if infogain > best_infogain:
             best_infogain = infogain
             best_attr = attr
-
     return best_attr
 
 def get_values(examples, attr):
+    '''
+    Return all possible values of the given attribute.
+    '''
     vals = []
     for example in examples:
-        if not vals or example[attr] not in vals:
+        if example[attr] not in vals:
             vals.append(example[attr])
     return vals
+
+def find_majority(examples):
+    '''
+    Return the majority calss value.
+    '''
+    val_freq = {}
+    majority_freq = 0.0
+    majority_val = None
+    for example in examples:
+        if example[CLASS_ATTR] in val_freq:
+            val_freq[example[CLASS_ATTR]] += 1.0
+        else:
+            val_freq[example[CLASS_ATTR]] = 1.0
+
+    for val, freq in val_freq.items():
+        if freq > majority_freq:
+            majority_freq = freq
+            majority_val = val
+
+    return majority_val
